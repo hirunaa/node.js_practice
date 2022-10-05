5aff59bae9b663ceb260a9ac7348144282c0c74c
hirunaa <tmfrl040622@naver.com>
2022-10-05 17:56:49 +0900

    ㅇㅇ

--- server/index.js
+++ server/index.js
@@ -45,14 +45,13 @@ app.post('/login', (req, res) => {
         }
         user.comparePassword(req.body.password, (err, isMatch) => {
             if(!isMatch)
-                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다.", 
-                /*user.generateToken((err, user) => {
+                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+                    user.generateToken((err, user) => {
                     if(err) return res.status(400).send(err);
                     res.cookie("x_auth", user.token)
                     .status(200)
                     .json({loginSuccess: true, userId: user._id})
-                })    */
-            })
+                })    
             
         })
         
